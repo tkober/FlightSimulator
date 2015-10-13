d0315121f27ebc3b1c61a5fcbeba5787411f1bff
Thorsten Kober <Thorsten.Kober@googlemail.com>
2015-10-13 22:41:55 +0200

    Extraction codes ausgelagert

---
+++ DisplayController/Extractions.h
@@ -0,0 +1,17 @@
+#define RADIO_AP_EXTRACTIONS                '='
+#define COM_1_FREQ_EXTRACTION               'A'
+#define COM_1_SB_FREQ_EXTRACTION            'B'
+#define COM_2_FREQ_EXTRACTION               'C'
+#define COM_2_SB_FREQ_EXTRACTION            'D'
+#define NAV_1_FREQ_EXTRACTION               'E'
+#define NAV_1_SB_FREQ_EXTRACTION            'F'
+#define NAV_2_FREQ_EXTRACTION               'G'
+#define NAV_2_SB_FREQ_EXTRACTION            'H'
+
+
+#define INDICATIONS_SYSTEM_1_EXTRACTIONS    '<'
+
+
+#define SYSTEM_2_OTHERS_EXTRACTIONS         '?'
+#define GEAR_POSITION_EXTRACTION            'Y'
+#define GEAR_RETRACTABLE_EXTRACTION         'b'                    
