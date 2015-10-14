eed7c93b176a469b2aa1c9aab10c6678bc65aa97
Thorsten Kober <Thorsten.Kober@googlemail.com>
2015-10-15 01:22:13 +0200

    MFDs angepasst.

--- DisplayController/MFD.h
+++ DisplayController/MFD.h
@@ -1,12 +1,31 @@
-struct MFDPage {
-  struct MFDPage *previous_page;
-  struct MFDPage *next_page;
-    
-  void (*will_appear)();
-  void (*render)();
-  void (*update_if_necessary)();
+#define MFD_PAGE_MEMBERS  void *previous_page;                  \
+                          void *next_page;                      \
+                                                                \
+                          void (*will_appear)();                \
+                          void (*render)();                     \
+                          void (*update_if_necessary)();        \
+                          void (*will_disappear)();             
+
+
+typedef struct MFDPage {
+  MFD_PAGE_MEMBERS
+} MFDPage;
+
+
+typedef struct RadioPage {
+  MFD_PAGE_MEMBERS
+  void (*change_button_pressed)();
+  void (*call)();
+  void (*swap)();
+  void (*increment)();
+  void (*decrement)();
+} RadioPage;
+
+
+typedef struct ECAMPage {
+  MFD_PAGE_MEMBERS
   void (*scroll_down)();
   void (*scroll_up)();
-  void (*will_disappear)();
-};
-typedef struct MFDPage MFDPage;
+  void (*handle_keypad_input)(char key);
+} ECAMPage;
+
