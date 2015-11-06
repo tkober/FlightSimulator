53eac4166e355db64915e2b06f76c0cd7c835b51
Thorsten Kober <kober@semvox.de>
2015-11-06 17:31:38 +0100

    AP weiter gemacht

--- DisplayController/AP.ino
+++ DisplayController/AP.ino
@@ -2,11 +2,31 @@
 #include "Pins.h"
 #include "MFD.h"
 #include "Glyphs.h"
+#include "SimConnectInputs.h"
 
 
 void update_ap1_display();
 void update_ap2_display();
 void update_status_leds();
+
+void incrementSpeed(int boost);
+void decrementSpeed(int boost);
+void toggleSpeedHold();
+
+void incrementHeading(int boost);
+void decrementHeading(int boost);
+void toggleHeadingHold();
+
+void incrementCourse(int boost);
+void decrementCourse(int boost);
+
+void incrementAltitude(int boost);
+void decrementAltitude(int boost);
+void toggleAlitiudeHold();
+
+void incrementVerticalSpeed(int boost);
+void decrementVerticalSpeed(int boost);
+void levelOff();
 
 
 #define AP1_LCD_ROW_COUNT   2
@@ -39,6 +59,12 @@ char a_thr_active[AP_COMPONENT_STATUS_SIZE]           = {'0'};
 LiquidCrystal ap_lcd_1 = LiquidCrystal(AP_LCD_1_PINS);
 LiquidCrystal ap_lcd_2 = LiquidCrystal(AP_LCD_2_PINS);
 
+PushableRotaryEncoder changeSpeedRotaryEncoder = PushableRotaryEncoder(AP_CHANGE_SPD_ENCODER_PIN_A, AP_CHANGE_SPD_ENCODER_PIN_B, AP_CHANGE_SPD_BUTTON_PIN, 1);
+PushableRotaryEncoder changeHeadingRotaryEncoder = PushableRotaryEncoder(AP_CHANGE_HDG_ENCODER_PIN_A, AP_CHANGE_HDG_ENCODER_PIN_B, AP_CHANGE_HDG_BUTTON_PIN, 1);
+PushableRotaryEncoder changeCourseRotaryEncoder = PushableRotaryEncoder(AP_CHANGE_CRS_ENCODER_PIN_A, AP_CHANGE_CRS_ENCODER_PIN_B, AP_CHANGE_CRS_BUTTON_PIN, 1);
+PushableRotaryEncoder changeAltitudeRotaryEncoder = PushableRotaryEncoder(AP_CHANGE_ALT_ENCODER_PIN_A, AP_CHANGE_ALT_ENCODER_PIN_B, AP_CHANGE_ALT_BUTTON_PIN, 1);
+PushableRotaryEncoder changeVerticalSpeedRotaryEncoder = PushableRotaryEncoder(AP_CHANGE_VS_ENCODER_PIN_A, AP_CHANGE_VS_ENCODER_PIN_B, AP_CHANGE_VS_BUTTON_PIN, 1);
+
 int ap1_updated = 0;
 int ap2_updated = 0;
 int status_updated = 0;
@@ -47,12 +73,33 @@ int status_updated = 0;
 // Public
 
 void ap_setup() {
+  changeSpeedRotaryEncoder.setOnRotateClockwise(incrementSpeed);
+  changeSpeedRotaryEncoder.setOnRotateCounterClockwise(decrementSpeed);
+  changeSpeedRotaryEncoder.setOnClick(toggleSpeedHold);
+    
+  changeHeadingRotaryEncoder.setOnRotateClockwise(incrementHeading);
+  changeHeadingRotaryEncoder.setOnRotateCounterClockwise(decrementHeading);
+  changeHeadingRotaryEncoder.setOnClick(toggleHeadingHold);
+    
+  changeCourseRotaryEncoder.setOnRotateClockwise(incrementCourse);
+  changeCourseRotaryEncoder.setOnRotateCounterClockwise(decrementCourse);
+    
+  changeAltitudeRotaryEncoder.setOnRotateClockwise(incrementAltitude);
+  changeAltitudeRotaryEncoder.setOnRotateCounterClockwise(decrementAltitude);
+  changeAltitudeRotaryEncoder.setOnClick(toggleAlitiudeHold);
+    
+  changeVerticalSpeedRotaryEncoder.setOnRotateClockwise(incrementVerticalSpeed);
+  changeVerticalSpeedRotaryEncoder.setOnRotateCounterClockwise(decrementVerticalSpeed);
+  changeVerticalSpeedRotaryEncoder.setOnClick(levelOff);
+    
   ap_lcd_1.createChar(ACTIVATED_INDICATOR_GLYPH_INDEX, ACTIVED_INDICATOR);
   ap_lcd_1.begin(AP1_LCD_COL_COUNT, AP1_LCD_ROW_COUNT);
   ap_lcd_2.createChar(ACTIVATED_INDICATOR_GLYPH_INDEX, ACTIVED_INDICATOR);
   ap_lcd_2.begin(AP2_LCD_COL_COUNT, AP2_LCD_ROW_COUNT);
+
   pinMode(AP_STATUS_PIN, OUTPUT);
   pinMode(A_THR_STATUS_PIN, OUTPUT);
+    
   update_ap1_display();
   update_ap2_display();
   update_status_leds();
@@ -60,6 +107,11 @@ void ap_setup() {
 
 
 void ap_tick() {
+  changeSpeedRotaryEncoder.tick();
+  changeHeadingRotaryEncoder.tick();
+  changeCourseRotaryEncoder.tick();
+  changeAltitudeRotaryEncoder.tick();
+  changeVerticalSpeedRotaryEncoder.tick();
   if (ap1_updated == 1) {
     ap1_updated = 0;
     update_ap1_display();
@@ -159,3 +211,77 @@ void update_status_leds() {
   digitalWrite(A_THR_STATUS_PIN, a_thr_active[0] == '1' ? HIGH : LOW);
 }
 
+
+// SPD
+void incrementSpeed(int boost) {
+  Serial.println(INCREMENT_REFERENCE_SPEED);
+}
+
+
+void decrementSpeed(int boost) {
+  Serial.println(DECREMENT_REFERENCE_SPEED);
+}
+
+
+void toggleSpeedHold() {
+  Serial.println(SPEED_HEADING_HOLD);
+}
+
+
+// HDG
+void incrementHeading(int boost) {
+  Serial.println(INCREMENT_REFERENCE_HEADING);
+}
+
+
+void decrementHeading(int boost) {
+  Serial.println(DECREMENT_REFERENCE_HEADING);
+}
+
+
+void toggleHeadingHold() {
+  Serial.println(TOGGLE_HEADING_HOLD);
+}
+
+
+// CRS
+void incrementCourse(int boost) {
+  Serial.println(INCREMENT_REFERENCE_COURSE);
+}
+
+
+void decrementCourse(int boost) {
+  Serial.println(DECREMENT_REFERENCE_COURSE);
+}
+
+
+// ALT
+void incrementAltitude(int boost) {
+  Serial.println(INCREMENT_REFERENCE_ALTITUDE);
+}
+
+
+void decrementAltitude(int boost) {
+  Serial.println(DECREMENT_REFERENCE_ALTITUDE);
+}
+
+
+void toggleAlitiudeHold() {
+  Serial.println(TOGGLE_ALTITUDE_HOLD);
+}
+
+
+// V/S
+void incrementVerticalSpeed(int boost) {
+  Serial.println(INCREMENT_REFERENCE_VERTICAL_SPEED);
+}
+
+
+void decrementVerticalSpeed(int boost) {
+  Serial.println(DECREMENT_REFERENCE_VERTICAL_SPEED);
+}
+
+
+void levelOff() {
+
+}
